9a513ba30b11134f40a6d488d79dc30ebddae2c5
sophia vincent <sophire33@gmail.com>
2025-01-28 15:59:36 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex text-blue-300 justify-center items-center'>
+        start
+      </div>
+    </>
+  )
+}
+
+export default App
