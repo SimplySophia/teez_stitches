5857a72992bebf832832e336c0c13b53ed08bf38
sophia vincent <sophire33@gmail.com>
2025-02-11 13:25:25 -0800

    setting services and Banner ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 
 
 import './App.css'
+import Banner from './components/Banner/Banner';
 import Category from './components/Category/Category';
 import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
+import Services from './components/Service/Services';
 
+const BannerData = {
+  discount: '25% OFF',
+  title: 'BOLD',
+  date: '10 Jan to 28 Jan',
+}
 
 function App() {
 
@@ -14,6 +21,8 @@ function App() {
         <Navbar />
         <Hero />
         <Category />
+        <Services />
+        <Banner />
       </div>
     </>
   )
--- src/components/Service/Services.jsx
+++ src/components/Service/Services.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
+import {
+  FaCarSide,
+  FaHeadphonesAlt,
+  FaWallet,
+  FaCheckCircle,
+} from "react-icons/fa";
 
+const ServiceData = [
+  {
+    id: 1,
+    icon: <FaCarSide className='text-4xl md:text-5xl text-red-600'/>,
+    title: "Free Suit",
+    description: "Free Shipping to all"
+  },
+  {
+    id: 2,
+    icon: <FaCheckCircle className='text-4xl md:text-5xl text-red-600'/>,
+    title: "Free Suit",
+    description: "Free Shipping to all"
+  },
+  {
+    id: 3,
+    icon: <FaHeadphonesAlt className='text-4xl md:text-5xl text-red-600'/>,
+    title: "Free Suit",
+    description: "Free Shipping to all"
+  },
+  {
+    id: 4,
+    icon: <FaWallet className='text-4xl md:text-5xl text-red-600'/>,
+    title: "Free Suit",
+    description: "Free Shipping to all"
+  },
+]
 const Services = () => {
   return (
-    <div>Services</div>
+    <div>
+      <div className='container mt-14 md:mt-20'>
+        <div className='grid grid-cols-2 lg:grid-cols-4 gap-4 gap-y-8'>
+          {ServiceData.map((data) => (
+            <div key={data} className='flex flex-col items-start sm:flex-row gap-4'>
+              {data.icon}
+              <div>
+                <h1 className='lg:text-xl font-bold'>{data.title}</h1>
+                <h1 className='text-gray-400 text-sm'>{data.description}</h1>
+              </div>
+            </div>
+          ))}
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
